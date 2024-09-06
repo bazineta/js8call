435bf3efdd5394e7f05900c8e2c6fe1a2f85a1e8
Allan Bazinet <allan.bazinet@gmail.com>
2024-09-06 06:39:18 -0700

    Eliminate dead store

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4973,7 +4973,7 @@ void MainWindow::readFromStdout(QProcess * proc)                             //r
 void MainWindow::processDecodedLine(QByteArray t){
   if(JS8_DEBUG_DECODE) qDebug() << "JS8: " << QString(t);
 
-  int navg=0;
+  //int navg=0;
 
   static QList<qint32> driftQueue;
 
@@ -5094,18 +5094,18 @@ void MainWindow::processDecodedLine(QByteArray t){
     return;
   }
 
-  if(m_mode=="JT4" or m_mode=="JT65" or m_mode=="QRA64" or m_mode=="FT8") {
-    int n=t.indexOf("f");
-    if(n<0) n=t.indexOf("d");
-    if(n>0) {
-      QString tt=t.mid(n+1,1);
-      navg=tt.toInt();
-      if(navg==0) {
-        char c = tt.data()->toLatin1();
-        if(int(c)>=65 and int(c)<=90) navg=int(c)-54;
-      }
-    }
-  }
+ // if(m_mode=="JT4" or m_mode=="JT65" or m_mode=="QRA64" or m_mode=="FT8") {
+ //   int n=t.indexOf("f");
+ //   if(n<0) n=t.indexOf("d");
+ //   if(n>0) {
+ //     QString tt=t.mid(n+1,1);
+ //     navg=tt.toInt();
+ //     if(navg==0) {
+ //       char c = tt.data()->toLatin1();
+ //       if(int(c)>=65 and int(c)<=90) navg=int(c)-54;
+ //     }
+ //   }
+ // }
 
   auto rawText = QString::fromUtf8 (t.constData ()).remove (QRegularExpression {"\r|\n"});
 
