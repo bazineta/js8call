2c1f9d5e2b32dc535b31e1c2b6a932a02f90946c
Allan Bazinet <allan.bazinet@gmail.com>
2024-09-13 10:49:58 -0700

    Use retrieved value

--- plotter.cpp
+++ plotter.cpp
@@ -247,8 +247,8 @@ void CPlotter::draw(float swide[], bool bScroll, bool)
   for(int i = 0; i < iz; i++)
   {
     float const y = swide[i];
-    if (y < ymin) ymin = y;
-    if (swide[i] < 1.e29) painter1.setPen(g_ColorTbl[std::clamp(static_cast<int>(10.0 * gain * y + m_plotZero), 0, 254)]);
+    if (y < ymin ) ymin = y;
+    if (y < 1.e29) painter1.setPen(g_ColorTbl[std::clamp(static_cast<int>(10.0 * gain * y + m_plotZero), 0, 254)]);
     painter1.drawPoint(i, m_j);
   }
 
