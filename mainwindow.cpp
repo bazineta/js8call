af1390b43a3a89b680674ec97da138e904d223ea
Allan Bazinet <allan.bazinet@gmail.com>
2024-10-01 10:14:22 -0700

    Note truncation issue

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3030,7 +3030,7 @@ void MainWindow::on_autoButton_clicked (bool checked)
   }
   if (!checked) m_bCallingCQ = false;
   statusUpdate ();
-  m_tAutoOn=DriftingDateTime::currentSecsSinceEpoch();
+  m_tAutoOn=DriftingDateTime::currentSecsSinceEpoch();  // XXX this a 64 into a 32
 
   // stop tx, reset the ui and message queue
   if(!checked){
