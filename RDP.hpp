c8107b5ba4821e471b91e82ec5ce25c1952ba02f
Allan Bazinet <allan.bazinet@gmail.com>
2024-12-03 05:38:21 -0800

    Commentary

--- RDP.hpp
+++ RDP.hpp
@@ -23,7 +23,20 @@ public:
 
   // Process the provided polygon through the Ramer–Douglas–Peucker
   // algorithm at the requested epsilon level, modifying it in-place
-  // and returning an iterator suitable for handing to polygon.erase().
+  // and returning an iterator suitable for erase-remove idiom usage,
+  // e.g.,
+  //
+  //   QPolygonF polygon;
+  //   RDP       rdp;
+  //
+  //   polygon.erase(rdp(polygon), polygon.end());
+  //
+  // Essentially, this acts the same as a std::remove_if() predicate
+  // does; points to retain are moved to the range [begin, iterator),
+  // while points to be elided are in the tail range [iterator, end].
+  // As the polygon remains the same size, the length of the tail is
+  // the number of elided points, and as with std::remove_if(), these
+  // points exist in memory but in an unspecified state.
 
   QPolygonF::iterator
   operator()(QPolygonF & polygon,
