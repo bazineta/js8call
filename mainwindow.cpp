0973d924af8f2a4738822f312bad9363484d0a96
Allan Bazinet <allan.bazinet@gmail.com>
2024-12-29 19:48:02 -0800

    Reduce parity matrix to simple function export

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -333,138 +333,87 @@ namespace
 
   constexpr auto parity = []()
   {
-    constexpr std::size_t Rows           = 87;
-    constexpr std::size_t Cols           = 87;
+    constexpr std::size_t                        Rows = 87;
+    constexpr std::size_t                        Cols = 87;
+    constexpr std::array<std::string_view, Rows> Data =
+    {
+      "23bba830e23b6b6f50982e", "1f8e55da218c5df3309052", "ca7b3217cd92bd59a5ae20",
+      "56f78313537d0f4382964e", "6be396b5e2e819e373340c", "293548a138858328af4210",
+      "cb6c6afcdc28bb3f7c6e86", "3f2a86f5c5bd225c961150", "849dd2d63673481860f62c",
+      "56cdaec6e7ae14b43feeee", "04ef5cfa3766ba778f45a4", "c525ae4bd4f627320a3974",
+      "41fd9520b2e4abeb2f989c", "7fb36c24085a34d8c1dbc4", "40fc3e44bb7d2bb2756e44",
+      "d38ab0a1d2e52a8ec3bc76", "3d0f929ef3949bd84d4734", "45d3814f504064f80549ae",
+      "f14dbf263825d0bd04b05e", "db714f8f64e8ac7af1a76e", "8d0274de71e7c1a8055eb0",
+      "51f81573dd4049b082de14", "d8f937f31822e57c562370", "b6537f417e61d1a7085336",
+      "ecbd7c73b9cd34c3720c8a", "3d188ea477f6fa41317a4e", "1ac4672b549cd6dba79bcc",
+      "a377253773ea678367c3f6", "0dbd816fba1543f721dc72", "ca4186dd44c3121565cf5c",
+      "29c29dba9c545e267762fe", "1616d78018d0b4745ca0f2", "fe37802941d66dde02b99c",
+      "a9fa8e50bcb032c85e3304", "83f640f1a48a8ebc0443ea", "3776af54ccfbae916afde6",
+      "a8fc906976c35669e79ce0", "f08a91fb2e1f78290619a8", "cc9da55fe046d0cb3a770c",
+      "d36d662a69ae24b74dcbd8", "40907b01280f03c0323946", "d037db825175d851f3af00",
+      "1bf1490607c54032660ede", "0af7723161ec223080be86", "eca9afa0f6b01d92305edc",
+      "7a8dec79a51e8ac5388022", "9059dfa2bb20ef7ef73ad4", "6abb212d9739dfc02580f2",
+      "f6ad4824b87c80ebfce466", "d747bfc5fd65ef70fbd9bc", "612f63acc025b6ab476f7c",
+      "05209a0abb530b9e7e34b0", "45b7ab6242b77474d9f11a", "6c280d2a0523d9c4bc5946",
+      "f1627701a2d692fd9449e6", "8d9071b7e7a6a2eed6965e", "bf4f56e073271f6ab4bf80",
+      "c0fc3ec4fb7d2bb2756644", "57da6d13cb96a7689b2790", "a9fa2eefa6f8796a355772",
+      "164cc861bdd803c547f2ac", "cc6de59755420925f90ed2", "a0c0033a52ab6299802fd2",
+      "b274db8abd3c6f396ea356", "97d4169cb33e7435718d90", "81cfc6f18c35b1e1f17114",
+      "481a2a0df8a23583f82d6c", "081c29a10d468ccdbcecb6", "2c4142bf42b01e71076acc",
+      "a6573f3dc8b16c9d19f746", "c87af9a5d5206abca532a8", "012dee2198eba82b19a1da",
+      "b1ca4ea2e3d173bad4379c", "b33ec97be83ce413f9acc8", "5b0f7742bca86b8012609a",
+      "37d8e0af9258b9e8c5f9b2", "35ad3fb0faeb5f1b0c30dc", "6114e08483043fd3f38a8a",
+      "cd921fdf59e882683763f6", "95e45ecd0135aca9d6e6ae", "2e547dd7a05f6597aac516",
+      "14cd0f642fc0c5fe3a65ca", "3a0a1dfd7eee29c2e827e0", "c8b5dffc335095dcdcaf2a",
+      "3dd01a59d86310743ec752", "8abdb889efbe39a510a118", "3f231f212055371cf3e2a2"
+    };
+
     constexpr std::size_t BitsPerElement = sizeof(std::uint64_t) * 8;
     constexpr std::size_t Elements       = (Rows * Cols + BitsPerElement - 1) / BitsPerElement;
 
-    // Compact bit matrix class, since we'd like a denser representation
-    // of this data than what a 2D std::array<bool> can provide; this is,
-    // after all, just bits. Consumers are provided only with our matrix
-    // access operator, since that's all they require.
+    std::array<std::uint64_t, Elements> matrix{};
 
-    class Matrix
+    for (std::size_t row = 0; row < Rows; ++row)
     {
-    private:
+      std::size_t col = 0;
       
-      // Data members
-
-      std::array<std::uint64_t, Elements> data{};
-
-      // Accessors
-
-      constexpr std::size_t
-      bitIndex(std::size_t const row,
-               std::size_t const col) const
+      for (auto const c : Data[row])
       {
-        return row * Cols + col;
-      }
-
-      constexpr std::size_t
-      elementIndex(std::size_t const row,
-                   std::size_t const col) const
-      {
-        return bitIndex(row, col) / BitsPerElement;
-      }
-
-      constexpr std::size_t
-      bitOffset(std::size_t const row,
-                std::size_t const col) const
-      {
-        return bitIndex(row, col) % BitsPerElement;
-      }
-
-      constexpr bool
-      get(std::size_t const row,
-          std::size_t const col) const
-      {
-        return (data[elementIndex(row, col)] >> bitOffset(row, col)) & 1;
-      }
-
-      // Manipulators
-
-      constexpr void
-      set(std::size_t const row,
-          std::size_t const col,
-          bool        const value)
-      {
-        auto &     element = data[elementIndex(row, col)];
-        auto const offset  =         bitOffset(row, col);
-
-        if (value) element |=  (uint64_t(1) << offset);
-        else       element &= ~(uint64_t(1) << offset);
-      }
-
-    public:
-
-      // Matrix access operator
-
-      constexpr bool operator()(std::size_t const row,
-                                std::size_t const col) const 
-      {
-        return get(row, col);
-      }
-
-      // Constructor
-
-      constexpr Matrix()
-      {
-        constexpr std::array<std::string_view, Rows> Data =
+        std::uint8_t value = 0;
+        
+        if      (c >= '0' && c <= '9') value = c - '0';
+        else if (c >= 'a' && c <= 'f') value = c - 'a' + 10;
+        else if (c >= 'A' && c <= 'F') value = c - 'A' + 10;
+        else throw "Invalid character in hex string";
+        
+        for (int i = 3; i >= 0; --i)
         {
-          "23bba830e23b6b6f50982e", "1f8e55da218c5df3309052", "ca7b3217cd92bd59a5ae20",
-          "56f78313537d0f4382964e", "6be396b5e2e819e373340c", "293548a138858328af4210",
-          "cb6c6afcdc28bb3f7c6e86", "3f2a86f5c5bd225c961150", "849dd2d63673481860f62c",
-          "56cdaec6e7ae14b43feeee", "04ef5cfa3766ba778f45a4", "c525ae4bd4f627320a3974",
-          "41fd9520b2e4abeb2f989c", "7fb36c24085a34d8c1dbc4", "40fc3e44bb7d2bb2756e44",
-          "d38ab0a1d2e52a8ec3bc76", "3d0f929ef3949bd84d4734", "45d3814f504064f80549ae",
-          "f14dbf263825d0bd04b05e", "db714f8f64e8ac7af1a76e", "8d0274de71e7c1a8055eb0",
-          "51f81573dd4049b082de14", "d8f937f31822e57c562370", "b6537f417e61d1a7085336",
-          "ecbd7c73b9cd34c3720c8a", "3d188ea477f6fa41317a4e", "1ac4672b549cd6dba79bcc",
-          "a377253773ea678367c3f6", "0dbd816fba1543f721dc72", "ca4186dd44c3121565cf5c",
-          "29c29dba9c545e267762fe", "1616d78018d0b4745ca0f2", "fe37802941d66dde02b99c",
-          "a9fa8e50bcb032c85e3304", "83f640f1a48a8ebc0443ea", "3776af54ccfbae916afde6",
-          "a8fc906976c35669e79ce0", "f08a91fb2e1f78290619a8", "cc9da55fe046d0cb3a770c",
-          "d36d662a69ae24b74dcbd8", "40907b01280f03c0323946", "d037db825175d851f3af00",
-          "1bf1490607c54032660ede", "0af7723161ec223080be86", "eca9afa0f6b01d92305edc",
-          "7a8dec79a51e8ac5388022", "9059dfa2bb20ef7ef73ad4", "6abb212d9739dfc02580f2",
-          "f6ad4824b87c80ebfce466", "d747bfc5fd65ef70fbd9bc", "612f63acc025b6ab476f7c",
-          "05209a0abb530b9e7e34b0", "45b7ab6242b77474d9f11a", "6c280d2a0523d9c4bc5946",
-          "f1627701a2d692fd9449e6", "8d9071b7e7a6a2eed6965e", "bf4f56e073271f6ab4bf80",
-          "c0fc3ec4fb7d2bb2756644", "57da6d13cb96a7689b2790", "a9fa2eefa6f8796a355772",
-          "164cc861bdd803c547f2ac", "cc6de59755420925f90ed2", "a0c0033a52ab6299802fd2",
-          "b274db8abd3c6f396ea356", "97d4169cb33e7435718d90", "81cfc6f18c35b1e1f17114",
-          "481a2a0df8a23583f82d6c", "081c29a10d468ccdbcecb6", "2c4142bf42b01e71076acc",
-          "a6573f3dc8b16c9d19f746", "c87af9a5d5206abca532a8", "012dee2198eba82b19a1da",
-          "b1ca4ea2e3d173bad4379c", "b33ec97be83ce413f9acc8", "5b0f7742bca86b8012609a",
-          "37d8e0af9258b9e8c5f9b2", "35ad3fb0faeb5f1b0c30dc", "6114e08483043fd3f38a8a",
-          "cd921fdf59e882683763f6", "95e45ecd0135aca9d6e6ae", "2e547dd7a05f6597aac516",
-          "14cd0f642fc0c5fe3a65ca", "3a0a1dfd7eee29c2e827e0", "c8b5dffc335095dcdcaf2a",
-          "3dd01a59d86310743ec752", "8abdb889efbe39a510a118", "3f231f212055371cf3e2a2"
-        };
-
-        for (std::size_t row = 0; row < Rows; ++row)
-        {
-          std::size_t bitIndex = 0;
-          
-          for (auto const c : Data[row])
+          if (col < Cols)
           {
-            std::uint8_t value = 0;
-            
-            if      (c >= '0' && c <= '9') value = c - '0';
-            else if (c >= 'a' && c <= 'f') value = c - 'a' + 10;
-            else if (c >= 'A' && c <= 'F') value = c - 'A' + 10;
-            else throw "Invalid character in hex string";
-            
-            for (int i = 3; i >= 0; --i)
+            if ((value >> i) & 1)
             {
-              if (bitIndex < Cols) set(row, bitIndex++, (value >> i) & 1);
+              auto const bitIndex     = row * Cols + col;
+              auto const elementIndex = bitIndex / BitsPerElement;
+              auto const bitOffset    = bitIndex % BitsPerElement;
+              
+              matrix[elementIndex] |= (uint64_t(1) << bitOffset);
             }
+            
+            col++;
           }
         }
       }
-    };
+    }
 
-    return Matrix();
+    return [matrix](std::size_t const row,
+                    std::size_t const col)
+    {
+      auto const bitIndex     = row * Cols + col;
+      auto const elementIndex = bitIndex / BitsPerElement;
+      auto const bitOffset    = bitIndex % BitsPerElement;
+
+      return (matrix[elementIndex] >> bitOffset) & 1;
+    };
   }();
 
   // Function that either translates valid JS8 message characters to their
