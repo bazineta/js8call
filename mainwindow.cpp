9b482028f850b7c7e7febae104c7892a9bc39cd1
Allan Bazinet <allan.bazinet@gmail.com>
2024-12-29 17:17:07 -0800

    Simplify matrix generation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -405,142 +405,66 @@ namespace
         return get(row, col);
       }
 
-      // Static constructor
+      // Constructor
 
-      static constexpr Matrix
-      from(std::array<std::array<bool, Cols>, Rows> const & data)
+      constexpr Matrix()
       {
-        Matrix result;
+        constexpr std::array<std::string_view, Rows> Data =
+        {
+          "23bba830e23b6b6f50982e", "1f8e55da218c5df3309052", "ca7b3217cd92bd59a5ae20",
+          "56f78313537d0f4382964e", "6be396b5e2e819e373340c", "293548a138858328af4210",
+          "cb6c6afcdc28bb3f7c6e86", "3f2a86f5c5bd225c961150", "849dd2d63673481860f62c",
+          "56cdaec6e7ae14b43feeee", "04ef5cfa3766ba778f45a4", "c525ae4bd4f627320a3974",
+          "41fd9520b2e4abeb2f989c", "7fb36c24085a34d8c1dbc4", "40fc3e44bb7d2bb2756e44",
+          "d38ab0a1d2e52a8ec3bc76", "3d0f929ef3949bd84d4734", "45d3814f504064f80549ae",
+          "f14dbf263825d0bd04b05e", "db714f8f64e8ac7af1a76e", "8d0274de71e7c1a8055eb0",
+          "51f81573dd4049b082de14", "d8f937f31822e57c562370", "b6537f417e61d1a7085336",
+          "ecbd7c73b9cd34c3720c8a", "3d188ea477f6fa41317a4e", "1ac4672b549cd6dba79bcc",
+          "a377253773ea678367c3f6", "0dbd816fba1543f721dc72", "ca4186dd44c3121565cf5c",
+          "29c29dba9c545e267762fe", "1616d78018d0b4745ca0f2", "fe37802941d66dde02b99c",
+          "a9fa8e50bcb032c85e3304", "83f640f1a48a8ebc0443ea", "3776af54ccfbae916afde6",
+          "a8fc906976c35669e79ce0", "f08a91fb2e1f78290619a8", "cc9da55fe046d0cb3a770c",
+          "d36d662a69ae24b74dcbd8", "40907b01280f03c0323946", "d037db825175d851f3af00",
+          "1bf1490607c54032660ede", "0af7723161ec223080be86", "eca9afa0f6b01d92305edc",
+          "7a8dec79a51e8ac5388022", "9059dfa2bb20ef7ef73ad4", "6abb212d9739dfc02580f2",
+          "f6ad4824b87c80ebfce466", "d747bfc5fd65ef70fbd9bc", "612f63acc025b6ab476f7c",
+          "05209a0abb530b9e7e34b0", "45b7ab6242b77474d9f11a", "6c280d2a0523d9c4bc5946",
+          "f1627701a2d692fd9449e6", "8d9071b7e7a6a2eed6965e", "bf4f56e073271f6ab4bf80",
+          "c0fc3ec4fb7d2bb2756644", "57da6d13cb96a7689b2790", "a9fa2eefa6f8796a355772",
+          "164cc861bdd803c547f2ac", "cc6de59755420925f90ed2", "a0c0033a52ab6299802fd2",
+          "b274db8abd3c6f396ea356", "97d4169cb33e7435718d90", "81cfc6f18c35b1e1f17114",
+          "481a2a0df8a23583f82d6c", "081c29a10d468ccdbcecb6", "2c4142bf42b01e71076acc",
+          "a6573f3dc8b16c9d19f746", "c87af9a5d5206abca532a8", "012dee2198eba82b19a1da",
+          "b1ca4ea2e3d173bad4379c", "b33ec97be83ce413f9acc8", "5b0f7742bca86b8012609a",
+          "37d8e0af9258b9e8c5f9b2", "35ad3fb0faeb5f1b0c30dc", "6114e08483043fd3f38a8a",
+          "cd921fdf59e882683763f6", "95e45ecd0135aca9d6e6ae", "2e547dd7a05f6597aac516",
+          "14cd0f642fc0c5fe3a65ca", "3a0a1dfd7eee29c2e827e0", "c8b5dffc335095dcdcaf2a",
+          "3dd01a59d86310743ec752", "8abdb889efbe39a510a118", "3f231f212055371cf3e2a2"
+        };
 
         for (std::size_t row = 0; row < Rows; ++row)
         {
-          for (std::size_t col = 0; col < Cols; ++col)
+          std::size_t bitIndex = 0;
+          
+          for (auto const c : Data[row])
           {
-            result.set(row, col, data[row][col]);
+            std::uint8_t value = 0;
+            
+            if      (c >= '0' && c <= '9') value = c - '0';
+            else if (c >= 'a' && c <= 'f') value = c - 'a' + 10;
+            else if (c >= 'A' && c <= 'F') value = c - 'A' + 10;
+            else throw "Invalid character in hex string";
+            
+            for (int i = 3; i >= 0; --i)
+            {
+              if (bitIndex < Cols) set(row, bitIndex++, (value >> i) & 1);
+            }
           }
         }
-
-        return result;
       }
     };
 
-    // Function to convert a hex string to a truth table row. This will be
-    // run only at compile time, so it doesn't need to be stellar in terms
-    // of efficiency, but it must be constexpr in C++17.
-
-    constexpr auto parseHexToBoolRow = [](const char * s)
-    {
-      std::array<bool, Cols> row{};
-      std::size_t bitIndex = 0;
-      
-      while (*s)
-      {
-        auto const   c = *s++;
-        std::uint8_t v = 0;
-        
-        if      (c >= '0' && c <= '9') v = c - '0';
-        else if (c >= 'a' && c <= 'f') v = c - 'a' + 10;
-        else if (c >= 'A' && c <= 'F') v = c - 'A' + 10;
-        else throw "Invalid character in hex string";
-        
-        for (int i = 3; i >= 0; --i)
-        {
-          if (bitIndex < 87) row[bitIndex++] = (v >> i) & 1;
-        }
-      }
-
-      return row;
-    };
-    
-    return Matrix::from(std::array<std::array<bool, Cols>, Rows>{{
-      parseHexToBoolRow("23bba830e23b6b6f50982e"),
-      parseHexToBoolRow("1f8e55da218c5df3309052"),
-      parseHexToBoolRow("ca7b3217cd92bd59a5ae20"),
-      parseHexToBoolRow("56f78313537d0f4382964e"),
-      parseHexToBoolRow("6be396b5e2e819e373340c"),
-      parseHexToBoolRow("293548a138858328af4210"),
-      parseHexToBoolRow("cb6c6afcdc28bb3f7c6e86"),
-      parseHexToBoolRow("3f2a86f5c5bd225c961150"),
-      parseHexToBoolRow("849dd2d63673481860f62c"),
-      parseHexToBoolRow("56cdaec6e7ae14b43feeee"),
-      parseHexToBoolRow("04ef5cfa3766ba778f45a4"),
-      parseHexToBoolRow("c525ae4bd4f627320a3974"),
-      parseHexToBoolRow("41fd9520b2e4abeb2f989c"),
-      parseHexToBoolRow("7fb36c24085a34d8c1dbc4"),
-      parseHexToBoolRow("40fc3e44bb7d2bb2756e44"),
-      parseHexToBoolRow("d38ab0a1d2e52a8ec3bc76"),
-      parseHexToBoolRow("3d0f929ef3949bd84d4734"),
-      parseHexToBoolRow("45d3814f504064f80549ae"),
-      parseHexToBoolRow("f14dbf263825d0bd04b05e"),
-      parseHexToBoolRow("db714f8f64e8ac7af1a76e"),
-      parseHexToBoolRow("8d0274de71e7c1a8055eb0"),
-      parseHexToBoolRow("51f81573dd4049b082de14"),
-      parseHexToBoolRow("d8f937f31822e57c562370"),
-      parseHexToBoolRow("b6537f417e61d1a7085336"),
-      parseHexToBoolRow("ecbd7c73b9cd34c3720c8a"),
-      parseHexToBoolRow("3d188ea477f6fa41317a4e"),
-      parseHexToBoolRow("1ac4672b549cd6dba79bcc"),
-      parseHexToBoolRow("a377253773ea678367c3f6"),
-      parseHexToBoolRow("0dbd816fba1543f721dc72"),
-      parseHexToBoolRow("ca4186dd44c3121565cf5c"),
-      parseHexToBoolRow("29c29dba9c545e267762fe"),
-      parseHexToBoolRow("1616d78018d0b4745ca0f2"),
-      parseHexToBoolRow("fe37802941d66dde02b99c"),
-      parseHexToBoolRow("a9fa8e50bcb032c85e3304"),
-      parseHexToBoolRow("83f640f1a48a8ebc0443ea"),
-      parseHexToBoolRow("3776af54ccfbae916afde6"),
-      parseHexToBoolRow("a8fc906976c35669e79ce0"),
-      parseHexToBoolRow("f08a91fb2e1f78290619a8"),
-      parseHexToBoolRow("cc9da55fe046d0cb3a770c"),
-      parseHexToBoolRow("d36d662a69ae24b74dcbd8"),
-      parseHexToBoolRow("40907b01280f03c0323946"),
-      parseHexToBoolRow("d037db825175d851f3af00"),
-      parseHexToBoolRow("1bf1490607c54032660ede"),
-      parseHexToBoolRow("0af7723161ec223080be86"),
-      parseHexToBoolRow("eca9afa0f6b01d92305edc"),
-      parseHexToBoolRow("7a8dec79a51e8ac5388022"),
-      parseHexToBoolRow("9059dfa2bb20ef7ef73ad4"),
-      parseHexToBoolRow("6abb212d9739dfc02580f2"),
-      parseHexToBoolRow("f6ad4824b87c80ebfce466"),
-      parseHexToBoolRow("d747bfc5fd65ef70fbd9bc"),
-      parseHexToBoolRow("612f63acc025b6ab476f7c"),
-      parseHexToBoolRow("05209a0abb530b9e7e34b0"),
-      parseHexToBoolRow("45b7ab6242b77474d9f11a"),
-      parseHexToBoolRow("6c280d2a0523d9c4bc5946"),
-      parseHexToBoolRow("f1627701a2d692fd9449e6"),
-      parseHexToBoolRow("8d9071b7e7a6a2eed6965e"),
-      parseHexToBoolRow("bf4f56e073271f6ab4bf80"),
-      parseHexToBoolRow("c0fc3ec4fb7d2bb2756644"),
-      parseHexToBoolRow("57da6d13cb96a7689b2790"),
-      parseHexToBoolRow("a9fa2eefa6f8796a355772"),
-      parseHexToBoolRow("164cc861bdd803c547f2ac"),
-      parseHexToBoolRow("cc6de59755420925f90ed2"),
-      parseHexToBoolRow("a0c0033a52ab6299802fd2"),
-      parseHexToBoolRow("b274db8abd3c6f396ea356"),
-      parseHexToBoolRow("97d4169cb33e7435718d90"),
-      parseHexToBoolRow("81cfc6f18c35b1e1f17114"),
-      parseHexToBoolRow("481a2a0df8a23583f82d6c"),
-      parseHexToBoolRow("081c29a10d468ccdbcecb6"),
-      parseHexToBoolRow("2c4142bf42b01e71076acc"),
-      parseHexToBoolRow("a6573f3dc8b16c9d19f746"),
-      parseHexToBoolRow("c87af9a5d5206abca532a8"),
-      parseHexToBoolRow("012dee2198eba82b19a1da"),
-      parseHexToBoolRow("b1ca4ea2e3d173bad4379c"),
-      parseHexToBoolRow("b33ec97be83ce413f9acc8"),
-      parseHexToBoolRow("5b0f7742bca86b8012609a"),
-      parseHexToBoolRow("37d8e0af9258b9e8c5f9b2"),
-      parseHexToBoolRow("35ad3fb0faeb5f1b0c30dc"),
-      parseHexToBoolRow("6114e08483043fd3f38a8a"),
-      parseHexToBoolRow("cd921fdf59e882683763f6"),
-      parseHexToBoolRow("95e45ecd0135aca9d6e6ae"),
-      parseHexToBoolRow("2e547dd7a05f6597aac516"),
-      parseHexToBoolRow("14cd0f642fc0c5fe3a65ca"),
-      parseHexToBoolRow("3a0a1dfd7eee29c2e827e0"),
-      parseHexToBoolRow("c8b5dffc335095dcdcaf2a"),
-      parseHexToBoolRow("3dd01a59d86310743ec752"),
-      parseHexToBoolRow("8abdb889efbe39a510a118"),
-      parseHexToBoolRow("3f231f212055371cf3e2a2")
-    }});
+    return Matrix();
   }();
 
   // Function that either translates valid JS8 message characters to their
