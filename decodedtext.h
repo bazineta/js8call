33c6f94206842bde824ba2af88d2465e718c7706
Allan Bazinet <allan.bazinet@gmail.com>
2025-01-31 05:57:32 -0800

    Simplify isDirected() logic

--- decodedtext.h
+++ decodedtext.h
@@ -26,24 +26,23 @@ public:
 
   // Inline accessors
 
-  int         bits()              const { return bits_;                   }
-  QString     compoundCall()      const { return compound_;               }
-  QStringList directedMessage()   const { return directed_;               }
-  float       dt()                const { return dt_;                     }
-  QString     extra()             const { return extra_;                  }
-  QString     frame()             const { return frame_;                  }
-  quint8      frameType()         const { return frameType_;              }
-  int         frequencyOffset()   const { return frequencyOffset_;        }
-  bool        isAlt()             const { return isAlt_;                  }
-  bool        isCompound()        const { return !compound_.isEmpty();    }
-  bool        isDirectedMessage() const { return !directed_.isEmpty() &&
-                                                  directed_.length() > 2; }
-  bool        isHeartbeat()       const { return isHeartbeat_;            }
-  bool        isLowConfidence ()  const { return isLowConfidence_;        }
-  QString     message()           const { return message_;                }
-  int         snr()               const { return snr_;                    }
-  int         submode()           const { return submode_;                }
-  int         time()              const { return time_;                   }
+  int         bits()              const { return bits_;                  }
+  QString     compoundCall()      const { return compound_;              }
+  QStringList directedMessage()   const { return directed_;              }
+  float       dt()                const { return dt_;                    }
+  QString     extra()             const { return extra_;                 }
+  QString     frame()             const { return frame_;                 }
+  quint8      frameType()         const { return frameType_;             }
+  int         frequencyOffset()   const { return frequencyOffset_;       }
+  bool        isAlt()             const { return isAlt_;                 }
+  bool        isCompound()        const { return !compound_.isEmpty();   }
+  bool        isDirectedMessage() const { return directed_.length() > 2; }
+  bool        isHeartbeat()       const { return isHeartbeat_;           }
+  bool        isLowConfidence ()  const { return isLowConfidence_;       }
+  QString     message()           const { return message_;               }
+  int         snr()               const { return snr_;                   }
+  int         submode()           const { return submode_;               }
+  int         time()              const { return time_;                  }
 
   // Accessors
 
@@ -55,11 +54,11 @@ private:
   // Unpacking strategies entry point and strategies.
 
   bool tryUnpack();
-  bool tryUnpackHeartbeat();
   bool tryUnpackCompound();
-  bool tryUnpackDirected();
   bool tryUnpackData();
+  bool tryUnpackDirected();
   bool tryUnpackFastData();
+  bool tryUnpackHeartbeat();
 
   // Specific unpacking strategies, attempted in order until one works
   // or all have failed.
