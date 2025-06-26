53c7be344b995c721374a8c0eb13cf8ed785b4f3
rruchte <rob@thirdpartylabs.com>
2025-06-26 18:21:02 -0400

    Changing group messaging delivery callbacks to use values instead of references

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9191,7 +9191,7 @@ void MainWindow::processCommandActivity() {
 				 */
 				if(!isGroupMsg)
 				{
-					callback = [this, mid, inbox, msg] (){
+					callback = [this, mid, msg] (){
 						this->markMsgDelivered(mid, msg);
 					};
 				}
@@ -9502,7 +9502,7 @@ int MainWindow::getNextGroupMessageIdForCallsign(QString group_name, QString cal
 }
 
 // Facade for Inbox::markGroupMsgDeliveredForCallsign
-bool MainWindow::markGroupMsgDeliveredForCallsign(int msgId, QString callsign)
+bool MainWindow::markGroupMsgDeliveredForCallsign(int msgId, const QString callsign)
 {
 	Inbox inbox(inboxPath());
 	if(!inbox.open()){
